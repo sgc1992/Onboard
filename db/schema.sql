1b53df21cd8ecaa40b06c00af79e6bb48b565f57
MarketingPlus <benmarketingplus@gmail.com>
2021-06-07 20:26:26 +1000

    added the schema.sql code

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+-- jawsdb schema for heroku deployment
+DROP DATABASE IF EXISTS onboard_db;
+CREATE DATABASE onboard_db;
+
+-- use this schema for local host
+DROP DATABASE IF EXISTS tu4j0moodwsbylle;
+CREATE DATABASE tu4j0moodwsbylle;

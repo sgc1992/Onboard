07092c3e7df315e3ef1cd97234023f78bc4a9860
MarketingPlus <benmarketingplus@gmail.com>
2021-06-08 18:39:34 +1000

    added a render homepage in homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,18 @@ const router = require('express').Router();
 const { Project, Todos, User } = require('../models');
 const withAuth = require('../utils/auth');
 
+// render homepage
+router.get('/', async (req, res) => {
+    try {
+        res.render('homepage', {
+            layout: 'main',
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 // render login/signup page
 router.get('/login'), (req, res => {
     if(req.session.logged_in) {
